82990ac9c8a08e39a851560e24077e3d920c36ba
dtaha04 <diyataha123456@gmail.com>
2026-09-15 12:14:21 -0700

    Add files via upload

---
+++ mcat-study-tracker/app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css'
+export const metadata={title:'MCAT Study Tracker',description:'Personal MCAT study dashboard'}
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
